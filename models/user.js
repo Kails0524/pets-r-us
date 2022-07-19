415df26da3ffeb255cc7af31c483832523852089
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-07-19 16:28:44 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -8,10 +8,7 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const UserSchema = new mongoose.Schema({
-  // username: { type: String, required: true, unique: true },
-  // password: { type: String, required: true },
-  // password: { type: String },
+let UserSchema = new mongoose.Schema({
   email: { type: String, required: true, lowercase: true },
   createdAt: { type: Date, default: Date.now },
 });
